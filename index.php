a6076c0c86ba65d7b09645e6cf25bd2cf113b693
haijunq <haijunq@gmail.com>
2014-04-24 22:00:23 -0700

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<style type="text/css">       
+#Video1
+{
+ position:absolute;
+ top: 50px;
+ left:0px;        
+ width:1000px;       
+ border:2px solid blue;
+ display:block;
+ z-index:99;
+ }
+
+
+</style>
+</head>
+<body>        
+   <video id="Video1" controls loop autoplay >
+       <source src="http://ie.microsoft.com/testdrive/ieblog/2011/nov/pp4_blog_demo.mp4" type="video/mp4" />           
+   </video> 
+</body>
+</html>
